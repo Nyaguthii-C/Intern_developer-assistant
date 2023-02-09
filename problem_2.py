cec0b5e284ec32e4ca237e87ce654eb8f06035ae
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-02-09 06:15:12 -0800

    add solution problem 2

--- problem_2.py
+++ problem_2.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 """
-
+Function to return max power needed for the list of fields and  water towers
 """
+tower = Towers[1, 4, 9]
+field = Fields[1, 2, 3, 4]
+distance = []
+
+
+def Waterpump_strength:
+    """
+    finds the distance between fields and water pumps
+    returns the greatest distance hence max power
+    """
+    for i in tower:
+        for j in field:
+            if i > j:
+                d = i - j
+        distance.append(d)
+    return max(distance)
